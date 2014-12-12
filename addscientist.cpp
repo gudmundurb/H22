e5fdd96bbc86da05290caa5caee612fea79fe20f
steinnp <steiniell@gmail.com>
2014-12-12 20:19:00 +0000

    addscientist

--- addscientist.cpp
+++ addscientist.cpp
@@ -12,3 +12,55 @@ AddScientist::~AddScientist()
 {
     delete ui;
 }
+
+Scientist AddScientist::getScientist(){
+    return newScientist;
+}
+
+
+void AddScientist::on_InputScientistName_textChanged(const QString &arg1)
+{
+    newScientist.name = ui->InputScientistName->text().toStdString();
+}
+
+void AddScientist::on_InputBirthYear_textChanged(const QString &arg1)
+{
+    newScientist.dateOfBirth = ui->InputBirthYear->text().toStdString();
+}
+
+void AddScientist::on_InputDeathYear_textChanged(const QString &arg1)
+{
+    newScientist.dateOfDeath = ui->InputDeathYear->text().toStdString();
+}
+void AddScientist::on_ScientistGender_currentIndexChanged(const QString &arg1)
+{
+    if(ui->ScientistGender->currentText().toStdString() == "Male"){
+        newScientist.gender = "M";
+    }
+    else{
+        newScientist.gender = "F";
+    }
+}
+
+
+
+void AddScientist::on_RadioStillAlive_toggled(bool checked)
+{
+    if(checked){
+        ui->InputDeathYear->setEnabled(false);
+        newScientist.dateOfDeath = "0";
+    }
+    else{
+        ui->InputDeathYear->setEnabled(true);
+    }
+}
+
+void AddScientist::on_Cancel_clicked()
+{
+    close();
+}
+
+void AddScientist::on_S_add_ok_clicked()
+{
+    close();
+}
--- addscientist.h
+++ addscientist.h
@@ -2,7 +2,7 @@
 #define ADDSCIENTIST_H
 
 #include <QWidget>
-
+#include "scientist.h"
 namespace Ui {
 class AddScientist;
 }
@@ -14,9 +14,25 @@ class AddScientist : public QWidget
 public:
     explicit AddScientist(QWidget *parent = 0);
     ~AddScientist();
+    Scientist getScientist();
+private slots:
+    void on_InputScientistName_textChanged(const QString &arg1);
+
+    void on_InputBirthYear_textChanged(const QString &arg1);
+
+    void on_InputDeathYear_textChanged(const QString &arg1);
+
+    void on_ScientistGender_currentIndexChanged(const QString &arg1);
+
+    void on_RadioStillAlive_toggled(bool checked);
+
+    void on_Cancel_clicked();
+
+    void on_S_add_ok_clicked();
 
 private:
     Ui::AddScientist *ui;
+    Scientist newScientist;
 };
 
 #endif // ADDSCIENTIST_H
